9645424bf39216f8ccd5740ccb502fb3db88742c
PhyTaffa <filippotaffarello@gmail.com>
2024-05-13 11:48:42 +0200

    Kinda works, missing the good iteration

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -1,4 +1,13 @@
-setInterval(getGameData, 2000);
+setInterval(getGameData, 3000);
+
+//var used to avoid using the playerId everywhere.
+const searchParams = new URLSearchParams(window.location.search);
+
+
+var playerId4 = searchParams.get('playerId');
+var matchId = searchParams.get('matchId');
+var CurrentColor = '';
+var playerColor = '';
 
 //funciton called on page loading and every 2 seconds
 function getGameData(){
@@ -6,10 +15,14 @@ function getGameData(){
     DisplayCard();
     DisplayShard();
     DisplayBoard();
+    console.log(this.playerId4);
+    console.log(this.matchId);
 }
 
 function DisplayName(){
     //Works for the match 1
+
+
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -18,23 +31,19 @@ function DisplayName(){
             var response = JSON.parse(xhttp.responseText);
 
             var user1Info = ' ';
-            var user2Info = ' ';
             var matchColor = ' ';
+            playerId = response[0].player_id;
 
 
             //all the infos for the player with the lowest id: name, color and, currently, id
             user1Info = response[0].player_name + '\n' + response[0].pc_name;
             document.getElementById("p1Name").innerText = user1Info;
-            document.getElementById("p1Id").innerText = 'player ID: ' + response[0].player_id;
+            document.getElementById("p1Id").innerText = 'playeR ID: ' + response[0].player_id;
             document.getElementById("p1NameForPromotion").innerText = response[0].player_name + "'s INVENTORY";
 
 
-
-            //all the infos for the player with the highest id: name, color and, currently, id
-            user2Info = response[1].player_name + '\n' + response[1].pc_name;
-            document.getElementById("p2Name").innerText = user2Info;
-            document.getElementById("p2Id").innerText = 'player ID: ' + response[1].player_id;
-            document.getElementById("p2NameForPromotion").innerText = response[1].player_name + "'s INVENTORY";
+            CurrentColor = response[0].match_pc_id;
+            playerColor = response[0].mp_pc_id;
 
 
 
@@ -57,9 +66,10 @@ function DisplayName(){
 
             //Shows up unntil which type the user can upgrade his pawns
             document.getElementById("utGoesHere").innerText = response[0].upgradeTier;
+            //onsole.log(response[0].upgradeTier);
         }
     };
-    xhttp.open("GET", "state/game/" + 1, true);
+    xhttp.open("GET", "state/game/" + matchId, true);
     xhttp.send()
 }
 
@@ -73,32 +83,19 @@ function DisplayCard(){
 
 
             //----------------WHITE INVERTORY--------------------------------------
-            document.getElementById("cardName1w").innerText = response[0].card_name;
-            document.getElementById("cardName2w").innerText = response[1].card_name;
-            document.getElementById("cardName3w").innerText = response[2].card_name;
-            document.getElementById("cardName4w").innerText = response[3].card_name;
+            document.getElementById("cardName1w").innerText = response[0].card_name + "(1)";
+            document.getElementById("cardName2w").innerText = response[1].card_name + "(2)";
+            document.getElementById("cardName3w").innerText = response[2].card_name + "(3)";
+            document.getElementById("cardName4w").innerText = response[3].card_name + "(4)";
 
             document.getElementById("cardQuantity1w").innerText = response[0].mpc_ammount;
             document.getElementById("cardQuantity2w").innerText = response[1].mpc_ammount;
             document.getElementById("cardQuantity3w").innerText = response[2].mpc_ammount;
             document.getElementById("cardQuantity4w").innerText = response[3].mpc_ammount;
 
-            //-------------BLACK INVETORY----------------------------------------- 
-
-            document.getElementById("cardName1b").innerText = response[0].card_name;
-            document.getElementById("cardName2b").innerText = response[1].card_name;
-            document.getElementById("cardName3b").innerText = response[2].card_name;
-            document.getElementById("cardName4b").innerText = response[3].card_name;
-
-            document.getElementById("cardQuantity1b").innerText = response[4].mpc_ammount;
-            document.getElementById("cardQuantity2b").innerText = response[5].mpc_ammount;
-            document.getElementById("cardQuantity3b").innerText = response[6].mpc_ammount;
-            document.getElementById("cardQuantity4b").innerText = response[7].mpc_ammount;
-
-
         }
     };
-    xhttp.open("GET", "state/card/1", true);
+    xhttp.open("GET", "state/card/" + matchId, true);
     xhttp.send()
 }
 
@@ -118,15 +115,9 @@ function DisplayShard(){
             document.getElementById("shardQuantity3w").innerText = response[2].mps_shard_ammount;
             document.getElementById("shardQuantity4w").innerText = response[3].mps_shard_ammount;
 
-            //-------------BLACK INVETORY----------------------------------------- 
-            document.getElementById("shardQuantity1b").innerText = response[4].mps_shard_ammount;
-            document.getElementById("shardQuantity2b").innerText = response[5].mps_shard_ammount;
-            document.getElementById("shardQuantity3b").innerText = response[6].mps_shard_ammount;
-            document.getElementById("shardQuantity4b").innerText = response[7].mps_shard_ammount;
-
         }
     };
-    xhttp.open("GET", "state/shard/1", true);
+    xhttp.open("GET", "state/shard/" + matchId, true);
     xhttp.send()
 }
 
@@ -198,12 +189,12 @@ function DisplayBoard(){
             }
             output += '\n-------------------------\n';
 
-            output += '1- 2- 3- 4- 5- 6- 7- 8';
+            output += '1 ‎ ‎ ‎ ‎ 2 ‎  3 ‎  4 ‎  5 ‎  6 ‎ 7 ‎ 8';
 
             document.getElementById("superbChessboard").innerText = output;
         }
     };
-    xhttp.open("GET", "state/boardR/1" , true);
+    xhttp.open("GET", "state/boardR/" + matchId , true);
     xhttp.send()
 }
 
@@ -213,7 +204,8 @@ function MakeMove() {
     var startY = document.getElementById('startY').value;
     var endX = document.getElementById('endX').value;
     var endY = document.getElementById('endY').value;
-    var playerId = document.getElementById('playerIdMove').value;
+    //gets the value of the player_id when the user info is loaded
+    var playerId = this.playerId
 
     // Construct JSON object with form data
     var data = {
@@ -222,10 +214,10 @@ function MakeMove() {
         endX: endX,
         endY: endY,
         playerId: playerId,
-        matchId: '1'
+        matchId: this.matchId
     };
 
-    console.log(JSON.stringify(data));
+    //console.log(JSON.stringify(data));
 
     // Create an XMLHttpRequest object
     var xhttp = new XMLHttpRequest();
@@ -239,7 +231,10 @@ function MakeMove() {
 
                 // Update the HTML element with the response
                 document.getElementById("responseMove").innerText = response;
-
+                document.getElementById('startX').value = '';
+                document.getElementById('startY').value = '';
+                document.getElementById('endX').value = '';
+                document.getElementById('endY').value = '';
                 // Log the response from the server
                 console.log(response);
                 getGameData();
@@ -261,14 +256,15 @@ function MakeMove() {
 function Promote(){
     var startX = document.getElementById('X').value;
     var startY = document.getElementById('Y').value;
-    var playerId = document.getElementById('PlayerIdPromote').value;
     var cardId = document.getElementById('cardId').value;
-
+    //gets the value of the player_id when the user info is loaded
+    var playerId = this.playerId;
+    
     var data = {
         startX: startX,
         startY: startY,
         playerId: playerId,
-        matchId: '1',
+        matchId: this.matchId,
         cardId: cardId,
     };
     console.log(JSON.stringify(data));
@@ -286,12 +282,19 @@ xhttp.onreadystatechange = function() {
 
             // Log the response from the server
             console.log(response);
+            document.getElementById('X').value = '';
+            document.getElementById('Y').value = '';
+            document.getElementById('cardId').value = '';
+
             getGameData();
         } else { // If there was an error with the request
             // Log the error status
             console.error("Error:", this.status);
         }
     }
+
+
+
 };
 
 // Configure the XMLHttpRequest object
